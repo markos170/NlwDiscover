e3e507f72a41ec8b011f9cc4934a8ba7454eb37c
Markos Souza <markossouza255@gmail.com>
2021-07-22 12:13:22 -0300

    atribuido novas rotas e funcionalidade de receber questions no db

--- src/route.js
+++ src/route.js
@@ -4,14 +4,18 @@ const RoomController = require('./controllers/RoomController')
 const route = express.Router()
 
 route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
+route.get('/creat-pass', (req, res) => res.render("index", {page: 'creat-pass'}))
 
+route.post('/creat-room', RoomController.creat)
 route.get('/room/:room', RoomController.open)
 
-route.get('/creat-pass', (req, res) => res.render("index", {page: 'creat-pass'}))
+
+
 
 //formato que o formulario de dentro da modal tem que passar para index
 route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/question/creat/:room', QuestionController.creat)
 
-route.post('/creat-room', RoomController.creat)
+
 
 module.exports = route
